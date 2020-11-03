da4e6365195c411b717fac84c1c6b8b15da78136
Derian2 <saldaer@mail.ru>
2020-11-03 15:39:14 +0300

    Fix

---
+++ Practical10/Practical10/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Practical10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practical10")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c113d8fd-47fd-4998-877b-690e7c98719f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Practical9(2)/Practical9(2)/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical9_2_
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+      DialogResult rsl = MessageBox.Show("Вы действительно хотите выйти из приложения ? ", "Внимание!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+      if (rsl == DialogResult.Yes)
+      { // выходим из приложения
+        Application.Exit();
+      }
+    }
+    Image MemForImage;
+
+    // функция загрузки изображения
+    private void LoadImage(bool jpg)
+    {
+
+      // директория, которая будет выбрана как начальная в окне для выбора файла 
+      openFileDialog1.InitialDirectory = "c:";
+
+      //еслибудемвыбирать jpg файлы
+      if (jpg)
+      {
+        // устанавливаем формат файлов для загрузки - jpg
+        openFileDialog1.Filter = "image (JPEG) files (*.jpg)|*.jpg|All files (*.*)|*.*";
+      }
+      else
+      {
+        // устанавливаем формат файлов для загрузки - png
+        openFileDialog1.Filter = "image (PNG) files (*.png)|*.png|All files (*.*)|*.*";
+      }
+
+      // если открытие окна выбора файла завершилось выбором файла и нажатием кнопки ОК 
+      if (openFileDialog1.ShowDialog() == DialogResult.OK)
+      {
+
+        try// безопасная попытка 
+        {
+          // пытаемся загрузить файл с именем openFileDialog1.FileName - выбранный пользователем файл. 
+          MemForImage = Image.FromFile(openFileDialog1.FileName);
+          // устанавливаем картинку в поле элемента PictureBox
+          pictureBox2.Image = MemForImage;
+        }
+        catch (Exception ex)// еслипопытказагрузкинеудалась
+        {
+          // выводим сообщение с причиной ошибки 
+          MessageBox.Show("Не удалось загрузить файл: " + ex.Message);
+        }
+
+      }
+
+    }
+
+
+    private void toolStripButton1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void toolStripButton2_Click(object sender, EventArgs e)
+    {
+      LoadImage(true);
+    }
+
+    private void toolStripButton3_Click(object sender, EventArgs e)
+    {
+      LoadImage(false);
+    }
+
+    private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void загрузитьИзображениеToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void вФорматеJPGToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      LoadImage(true);
+    }
+
+    private void вФорматеPNGToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      LoadImage(false);
+    }
+
+    private void pictureBox2_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
